a304bf1a0851b11a475588bf374f6aef2a89f452
Kishan Silawat <kishan.silawat@skyflow.com>
2022-11-10 18:28:05 +0530

    Updated for job status update

--- DataTransferNotifier.go
+++ DataTransferNotifier.go
@@ -13,12 +13,14 @@ import (
 )
 
 type JobDetail struct {
-	Id          string `json:"id"`
-	JobId       string `json:"jobId"`
-	JobStatus   string `json:"jobStatus"`
-	RequestId   string `json:"requestId"`
-	Query       string `json:"query"`
-	Destination string `json:"destination"`
+	Id                 string `json:"id"`
+	JobId              string `json:"jobId"`
+	JobStatus          string `json:"jobStatus"`
+	RequestId          string `json:"requestId"`
+	Query              string `json:"query"`
+	Destination        string `json:"destination"`
+	TaskExecutionArn   string `json:"taskExecutionArn"`
+	DataTransferStatus string `json:"dataTransferStatus"`
 }
 
 var db *sql.DB
@@ -48,7 +50,7 @@ func main() {
 func GetJobDetail(taskExecutionArn string) (JobDetail, error) {
 	log.Printf("Initiating GetJobDetail with taskExecutionArn:%v", taskExecutionArn)
 
-	statement := `SELECT id, jobid, jobstatus, requestid, query, destination FROM emr_job_details WHERE task_execution_arn=$1`
+	statement := `SELECT id, jobid, jobstatus, requestid, query, destination, task_execution_arn, data_transfer_state FROM emr_job_details WHERE task_execution_arn=$1`
 	var jobDetail JobDetail
 
 	record := db.QueryRow(statement, taskExecutionArn)
@@ -60,6 +62,8 @@ func GetJobDetail(taskExecutionArn string) (JobDetail, error) {
 		&jobDetail.RequestId,
 		&jobDetail.Query,
 		&jobDetail.Destination,
+		&jobDetail.TaskExecutionArn,
+		&jobDetail.DataTransferStatus,
 	); err {
 	case sql.ErrNoRows:
 		return jobDetail, sql.ErrNoRows
@@ -73,24 +77,39 @@ func GetJobDetail(taskExecutionArn string) (JobDetail, error) {
 func UpdateJob(jobDetail JobDetail, updatedDataTransferStatus string) {
 	log.Printf("%v-> Data Sync: Initiating UpdateJob", jobDetail.Id)
 
-	statement := `UPDATE emr_job_details SET data_transfer_state = $1 WHERE jobid = $2;`
+	statement := `UPDATE emr_job_details SET data_transfer_state = $1 WHERE id = $2;`
 
-	log.Printf("%v-> Data Sync: Updating record for arnId: %v\n", jobDetail.Id, jobDetail.JobId)
+	log.Printf("%v-> Data Sync: Updating record for taskExecutionArn: %v\n", jobDetail.Id, jobDetail.TaskExecutionArn)
 
-	_, err := db.Exec(statement, updatedDataTransferStatus, jobDetail.JobId)
+	_, err := db.Exec(statement, updatedDataTransferStatus, jobDetail.Id)
 
 	if err != nil {
-		log.Printf("%v-> Data Sync: Failed to update record for jobId: %v with error: %v\n", jobDetail.Id, jobDetail.JobId, err.Error())
+		log.Printf("%v-> Data Sync: Failed to update record for taskExecutionArn: %v with error: %v\n", jobDetail.Id, jobDetail.TaskExecutionArn, err.Error())
 		return
 	}
 
-	log.Printf("%v-> Data Sync: Successfully Updated jobStatus: %v for jobId: %v\n", jobDetail.Id, updatedDataTransferStatus, jobDetail.JobId)
+	log.Printf("%v-> Data Sync: Successfully Updated dataTransferStatus: %v for taskExecutionArn: %v\n", jobDetail.Id, updatedDataTransferStatus, jobDetail.TaskExecutionArn)
+
+	if updatedDataTransferStatus == "SUCCESS" {
+		statement := `UPDATE emr_job_details SET jobstatus = $1 WHERE id = $2;`
+
+		log.Printf("%v-> Data Sync: Updating record for id: %v\n", jobDetail.Id, jobDetail.Id)
+
+		_, err := db.Exec(statement, updatedDataTransferStatus, jobDetail.Id)
+
+		if err != nil {
+			log.Printf("%v-> Data Sync: Failed to update record for id: %v with error: %v\n", jobDetail.Id, jobDetail.Id, err.Error())
+			return
+		}
+
+		log.Printf("%v-> Data Sync: Successfully Updated jobstatus: %v for id: %v\n", jobDetail.Id, updatedDataTransferStatus, jobDetail.Id)
+	}
 }
 
 func HandleEvent(event events.CloudWatchEvent) {
 	if event.Source == "aws.datasync" {
 
-		log.Printf("Initiating data sync Notifier\n")
+		log.Printf("Initiating Data Transfer Notifier\n")
 
 		var eventContext map[string]interface{}
 		if err := json.Unmarshal(event.Detail, &eventContext); err != nil {
